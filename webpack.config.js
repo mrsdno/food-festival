6e0664f7b7a55ceb990f14825618f21afdde8a8c
mrsdno <christine@dno.dev>
2022-07-10 20:12:28 -0400

    automated the optimization phase

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   // root of the bundle and the beginning of the dependency graph
   // give it the relative path to the clients code
-  entry: "./assets/js/script.js",
+  // add multiple entry points if you are splitting application into separate modules
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js",
+  },
 
   // specify the output folder
   // best practice to bundle into folder named dist for 'distribution
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+
+  // regex to process any image file with the file extention .jpg
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name(file) {
+                return "[path][name].[ext]";
+              },
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
   },
 
   // Whenever you work with libraries that are dependent on the use of a global variable
@@ -19,6 +53,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
     }),
   ],
 
